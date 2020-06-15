fa98db6b5bb501b0f55c862fb4b168409c7dc247
stap780 <panaet80@mail.ru>
2020-06-15 16:55:30 +0300

    change

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.14.0"
+lock "~> 3.0.1"
 
 # set :application, "my_app_name"
 # set :repo_url, "git@example.com:me/my_repo.git"
