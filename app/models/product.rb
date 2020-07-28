5742bb6efbba155411e363ce0c6986dfae3e3682
stap780 <panaet80@mail.ru>
2020-07-28 12:15:54 +0300

    change param

--- app/models/product.rb
+++ app/models/product.rb
@@ -503,7 +503,7 @@ class Product < ApplicationRecord
     values = vparamHeader.uniq
     values.each do |value|
       puts "параметр - "+"#{value}"
-      url = "http://"+<%= ENV["INSALES_LOGIN"] %>+":"+<%= ENV["INSALES_PASS"] %>+"@"+<%= ENV["INSALES_DOMAIN"] %>+"/admin/properties.json"
+      url = "http://"+ ENV["INSALES_LOGIN"] +":"+ ENV["INSALES_PASS"] +"@"+ ENV["INSALES_DOMAIN"] +"/admin/properties.json"
         data = 	{
             "property":
                   {
