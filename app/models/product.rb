a16cfe39f532646dda37438493a94fea3d219a68
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-02-14 13:50:20 +0300

    chore text

--- app/models/product.rb
+++ app/models/product.rb
@@ -96,7 +96,7 @@ class Product < ApplicationRecord
     products = Product.where.not(provider: nil).where.not(productid_provider: nil).order(:id)
 
     CSV.open("#{Rails.root}/public/export_insales.csv", "wb") do |writer|
-      headers = [ 'ID варианта товара', 'Артикул', 'Название товара', 'Цена продажи', 'Остаток', 'Склад' ]
+      headers = [ 'ID варианта товара', 'Артикул', 'Название товара', 'Цена продажи', 'Остаток', 'Склад Удаленный' ]
 
       writer << headers
       products.each do |pr|
